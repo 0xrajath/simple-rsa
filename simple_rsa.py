2fa50bbb6232f029086e4f1af8f93323e546583a
rajathalex <rajathalex@gmail.com>
2018-11-28 23:56:10 -0500

    RSA System completed

--- simple_rsa.py
+++ simple_rsa.py
@@ -8,6 +8,7 @@ def random_bit():
 
     return lsb_bit,bin_rand_num,rand_num
 
+
 # Function to get 7-bit random number
 def random_num_7bit():
     rand_num = 1 # b_7
@@ -21,9 +22,11 @@ def random_num_7bit():
     rand_num = rand_num + 1 #b_0
     return rand_num
 
+
 # Function to get 'a' for Miller-Rabin
 def get_a(n):
     return random.randint(1,n-1)
+
 
 # Miller-Rabin Primality Testing
 def primality_testing(n,a):
@@ -49,6 +52,7 @@ def primality_testing(n,a):
     else:
         return True
 
+
 # Function to check if Prime using 20 rounds of Miller-Rabin
 def is_prime(n):
     for i in range(20):
@@ -58,6 +62,7 @@ def is_prime(n):
             return False
     # It is Prime if it passes for 20 a's
     return True
+
 
 # Extended Euclidean Algorithm to get Multiplicative Inverse and GCD
 def extended_euclidean_algorithm(bigger_num, smaller_num):
@@ -107,7 +112,14 @@ def extended_euclidean_algorithm(bigger_num, smaller_num):
             ti_1 = ti
 
         if ri_1 == 0:
-            return ti,ri #Return (Multiplicative Inverse, GCD)
+            found_mult_inverse = False
+            if ri==1:
+                found_mult_inverse = True
+                while ti < 0: # Normalizing Multiplicative Inverse
+                    ti = ti + bigger_num
+
+
+            return ti,ri,found_mult_inverse #Return (Multiplicative Inverse, GCD, Yes/No Multiplicative Inverse)
 
         qi = int(ri/ri_1)
         ri_2 = ri%ri_1
@@ -115,6 +127,53 @@ def extended_euclidean_algorithm(bigger_num, smaller_num):
         i+=1
         ri = ri_1
         ri_1 = ri_2
+
+
+# Function to generate RSA Keys. Public Key: (n,e) ; Private Key: (n,d)
+def generate_rsa_keys():
+    found_e = False
+
+    while not found_e: # In the rare case that an e was not found
+        #Calculating p and q
+        p = -1
+        q = -1
+        
+        #Assigning p
+        while True:
+            n = random_num_7bit()
+            if is_prime(n):
+                p = n
+                break
+        
+        #Assigning q
+        while True:
+            n = random_num_7bit()
+            if is_prime(n):
+                q = n
+                if q != p: #Checking that p and q are not equal
+                    break
+        
+        #Calculating n
+        n = p*q
+        #Calculating phi(n)
+        phi_n = (p-1)*(q-1)
+
+        e = 3 #Starting to check with e=3
+        d = -1
+        found_mult_inverse = False
+
+        while (not found_mult_inverse) and e < phi_n:
+            mult_inv,gcd,found_mult_inverse = extended_euclidean_algorithm(phi_n,e)
+
+            if not found_mult_inverse:
+                e+=1
+            else: # Found Multiplicative Inverse as GCD = 1
+                found_e = True
+                d = mult_inv
+
+    return p,q,n,e,d
+    
+    
 
 
 def main():
@@ -247,7 +306,6 @@ def main():
                 p = n
                 break
     
-    print("p:",p)
     #Assigning q
     while True:
         n = random_num_7bit()
@@ -256,78 +314,133 @@ def main():
             if q != p: #Checking that p and q are not equal
                 break
     
-    print("q",q)
     #Calculating n
     n = p*q
-    print("n:",n)
+
     #Calculating phi(n)
     phi_n = (p-1)*(q-1)
-    print("phi(n):",phi_n)
 
 
-    # Extended Euclidean Algorithm
-    ri = 5904
-    ri_1 = 5
-    ri_2 = -1
+    # Line 162: Finding e and d
+    print("line:162")
 
-    qi = -1
-    qi_1 = -1
-    qi_2 = -1
+    e = 3 #Starting to check with e=3
+    d = -1
+    found_mult_inverse = False
 
-    si = -1
-    si_1 = -1
-    si_2 = -1
+    while not found_mult_inverse:
+        print("e = "+str(e))
+        # Extended Euclidean Algorithm
+        ri = phi_n
+        ri_1 = e
+        ri_2 = -1
 
-    ti = -1
-    ti_1 = -1
-    ti_2 = -1
+        qi = -1
+        qi_1 = -1
+        qi_2 = -1
 
-    i = 1
+        si = -1
+        si_1 = -1
+        si_2 = -1
 
-    print("i |qi |r |ri+1 |ri+2 |si |ti")
-    while True:
-        if i == 1:
-            si_2 = 1
-            si = 1
+        ti = -1
+        ti_1 = -1
+        ti_2 = -1
 
-            ti_2 = 0
-            ti = 0
-        elif i == 2:
-            qi_1 = qi
+        i = 1
 
-            si_1 = 0
-            si = 0
+        print("i |qi |r |ri+1 |ri+2 |si |ti")
+        while True:
+            if i == 1:
+                si_2 = 1
+                si = 1
 
-            ti_1 = 1
-            ti = 1
-        else:
-            qi_2 = qi_1
-            qi_1 = qi
+                ti_2 = 0
+                ti = 0
+            elif i == 2:
+                qi_1 = qi
 
-            si = si_2 - (qi_2*si_1)
-            si_2 = si_1
-            si_1 = si
+                si_1 = 0
+                si = 0
 
-            ti = ti_2 - (qi_2*ti_1)
-            ti_2 = ti_1
-            ti_1 = ti
+                ti_1 = 1
+                ti = 1
+            else:
+                qi_2 = qi_1
+                qi_1 = qi
 
-        if ri_1 == 0:
-            print(str(i)+" |  |"+str(ri)+" |  |  |"+str(si)+" |"+str(ti))
-            break
+                si = si_2 - (qi_2*si_1)
+                si_2 = si_1
+                si_1 = si
 
-        qi = int(ri/ri_1)
-        ri_2 = ri%ri_1
-        print(str(i)+" |"+str(qi)+" |"+str(ri)+" |"+str(ri_1)+" |"+str(ri_2)+" |"+str(si)+" |"+str(ti))
+                ti = ti_2 - (qi_2*ti_1)
+                ti_2 = ti_1
+                ti_1 = ti
 
-        i+=1
-        ri = ri_1
-        ri_1 = ri_2
+            if ri_1 == 0:
+                
+                if ri==1:
+                    found_mult_inverse = True
+                    while ti < 0: # Normalizing Multiplicative Inverse
+                        ti = ti + phi_n
+                    d = ti
+
+                print(str(i)+" |  |"+str(ri)+" |  |  |"+str(si)+" |"+str(ti))
+                break
+
+            qi = int(ri/ri_1)
+            ri_2 = ri%ri_1
+            print(str(i)+" |"+str(qi)+" |"+str(ri)+" |"+str(ri_1)+" |"+str(ri_2)+" |"+str(si)+" |"+str(ti))
+
+            i+=1
+            ri = ri_1
+            ri_1 = ri_2
+
+        if not found_mult_inverse:
+            e+=1
 
 
+    # Empty Line
     print()
-    mi,gcd = extended_euclidean_algorithm(4620,13)
-    print(mi,gcd)
+
+
+    # Line 173: Printing d
+    print("line:173")
+    print("d = "+str(d))
+
+
+    # Empty Line
+    print()
+
+
+    # Line 177: Printing Alice's Keys
+    print("line:177")
+    print("p = "+str(p)+", q = "+str(q)+", n = "+str(n)+", e = "+str(e)+", d = "+str(d))
+    print("p = "+format(p, '032b'))
+    print("q = "+format(q, '032b'))
+    print("n = "+format(n, '032b'))
+    print("e = "+format(e, '032b'))
+    print("d = "+format(d, '032b'))
+
+
+    # Empty Line
+    print()
+
+
+    # Line 185: Printing Trent's Keys
+    print("line:185")
+    pt,qt,nt,et,dt = generate_rsa_keys()
+    print("p = "+str(pt)+", q = "+str(qt)+", n = "+str(nt)+", e = "+str(et)+", d = "+str(dt))
+    print("p = "+format(pt, '032b'))
+    print("q = "+format(qt, '032b'))
+    print("n = "+format(nt, '032b'))
+    print("e = "+format(et, '032b'))
+    print("d = "+format(dt, '032b'))
+
+
+    # Empty Line
+    print()
+
 
 
 
